a6698d666db2e6711ce9a2fb9a6c97ab7622ee5a
Jacob Fowles <js.fowles@gmail.com>
2016-05-23 21:26:45 -0600

    working basic command function

--- ruby_sinatra.rb
+++ ruby_sinatra.rb
@@ -9,7 +9,7 @@ end
 get '/birthday' do
 	@birthday_topics = {'What day is your birthday?': '/day',
 		 									'What do you want for your birthday?': '/want',
-											'How old are you': '/old',
+											'How old are you': '/old', 'Basic Commands': '/basic',
 											'BACK': '/menu'}
 	erb :birthday
 end
@@ -33,7 +33,7 @@ get '/school' do
 	@school_topics = {'Did you have fun at school today?': '/fun',
 		 								'What did you learn about?': '/learn',
 										'Did you listen to your teacher': '/listen',
-										'BACK': '/menu'}
+										'Basic Commands': '/basic', 'BACK': '/menu'}
 	erb :school
 end
 
@@ -53,35 +53,60 @@ get '/listen' do
 end
 
 
-	get '/behavior' do
-		@behav_topics = {'Were you good today?': '/good',
-			 								'Were you nice to every one you met today?': '/nice',
-											'Did you listen to our parents?': '/parents',
-											'BACK': '/menu'}
-		erb :behavior
-	end
+get '/behavior' do
+	@behav_topics = {'Were you good today?': '/good',
+		 								'Were you nice to every one you met today?': '/nice',
+										'Did you listen to our parents?': '/parents',
+										'Basic Commands': '/basic', 'BACK': '/menu'}
+	erb :behavior
+end
 
-	get '/good' do
-		`say were you good today?`
-		redirect '/behavior'
-	end
+get '/good' do
+	`say were you good today?`
+	redirect '/behavior'
+end
 
-	get '/nice' do
-		`say where you nice to everyone you met today?`
-		redirect '/behavior'
-	end
+get '/nice' do
+	`say where you nice to everyone you met today?`
+	redirect '/behavior'
+end
 
-	get '/parents' do
-		`say did you listen to your parents?`
-		redirect '/behavior'
-	end
+get '/parents' do
+	`say did you listen to your parents?`
+	redirect '/behavior'
+end
 
-	get '/menu' do
-		@menu_options = {'Birthday': '/birthday',
-			'School': '/school',
-			'Behavior': '/behavior'}
-			erb :menu
-		end
+get '/basic' do
+	@basic_topics = {'That is awesome!': '/awesome',
+		 								'Way to go buddy!': '/buddy',
+										'You are a crazy silly pants': '/silly',
+										'BACK': '/menu'}
+	erb :basic
+end
+
+get '/awesome' do
+	`say that is awesome!`
+	redirect '/basic'
+end
+
+get '/buddy' do
+	`say way to go buddy!`
+	redirect '/basic'
+end
+
+get '/silly' do
+	`say You are a crazy silly pants!`
+	redirect '/basic'
+end
+
+
+get '/menu' do
+	@menu_options = {'Birthday': '/birthday',
+									'School': '/school',
+									'Behavior': '/behavior',
+									'Basic Commands': '/basic'}
+		erb :menu
+end
 
 post '/' do
 	@name = params[:name]
