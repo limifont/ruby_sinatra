f63662c86ec5afa86e645567d027f3c18cbe879d
Lindsey <lindsey.font@gmail.com>
2016-05-24 04:10:05 -0600

    buddy change

--- ruby_sinatra.rb
+++ ruby_sinatra.rb
@@ -1,12 +1,22 @@
 require 'sinatra'
+require 'sinatra/cookies'
 require 'pry'
 require 'babbler'
 
-@buddy = 'victoria'
 
 
 get '/' do
+	buddy_name = cookies[:buddy]
+	if buddy_name == 'alex'
+		buddy_name == 'Bob'
+	end
+	`say -v #{cookies[:buddy]} Hi! My name is #{buddy_name}. What is your name?`
 	erb :home
+end
+
+get '/pick-a-buddy/:name' do
+	cookies[:buddy] = params[:name]
+	redirect '/'
 end
 
 
@@ -16,7 +26,7 @@ get '/menu' do
 	@topics = {'Birthday': '/birthday',
 				'School': '/school',
 				'Behavior': '/behavior',
-				'Basic Commands': '/basic'
+				'Basic Commands': '/basic',
 				'RETURN TO MAIN MENU': '/'}
 	erb :topics
 end
@@ -26,23 +36,23 @@ get '/birthday' do
 	@topics = {'What day is your birthday?': '/day',
 				'What do you want for your birthday?': '/want',
 				'How old are you?': '/old',
-				'Basic Commands': '/basic'
+				'Basic Commands': '/basic',
 				'BACK': '/menu'}
 	erb :topics
 end
 
 get '/day' do
-	`say -v ` + @buddy + `What day is your birthday`
+	system("say -v #{cookies[:buddy]} What day is your birthday")
 	redirect '/birthday'
 end
 
 get '/want' do
-	`say What do you want for your birthday`
+	system("say -v #{cookies[:buddy]} What do you want for your birthday")
 	redirect '/birthday'
 end
 
 get '/old' do
-	`say how old are you`
+	`say -v #{cookies[:buddy]} how old are you`
 	redirect '/birthday'
 end
 
@@ -56,17 +66,17 @@ get '/school' do
 end
 
 get '/fun' do
-	`say did you have fun at school today`
+	`say -v #{cookies[:buddy]} did you have fun at school today`
 	redirect '/school'
 end
 
 get '/learn' do
-	`say what did you learn about`
+	`say -v #{cookies[:buddy]} what did you learn about`
 	redirect '/school'
 end
 
 get '/listen' do
-	`say did you listen to your teacher`
+	`say -v #{cookies[:buddy]} did you listen to your teacher`
 	redirect '/school'
 end
 
@@ -80,48 +90,48 @@ get '/behavior' do
 end
 
 get '/good' do
-	`say were you good today?`
+	`say -v #{cookies[:buddy]} were you good today?`
 	redirect '/behavior'
 end
 
 get '/nice' do
-	`say where you nice to everyone you met today?`
+	`say -v #{cookies[:buddy]} where you nice to everyone you met today?`
 	redirect '/behavior'
 end
 
 get '/parents' do
-	`say did you listen to your parents?`
+	`say -v #{cookies[:buddy]} did you listen to your parents?`
 	redirect '/behavior'
 end
 
 
 get '/basic' do
-	@basic_topics = {'That is awesome!': '/awesome',
+	@topics = {'That is awesome!': '/awesome',
 		 			'Way to go buddy!': '/buddy',
 					'You are a crazy silly pants': '/silly',
 					'BACK': '/menu'}
-	erb :basic
+	erb :topics
 end
 
 get '/awesome' do
-	`say that is awesome!`
+	`say -v #{cookies[:buddy]} that is awesome!`
 	redirect '/basic'
 end
 
 get '/buddy' do
-	`say way to go buddy!`
+	`say -v #{cookies[:buddy]} way to go buddy!`
 	redirect '/basic'
 end
 
 get '/silly' do
-	`say You are a crazy silly pants!`
+	`say -v #{cookies[:buddy]} You are a crazy silly pants!`
 	redirect '/basic'
 end
 
 
 post '/' do
 	@name = params[:name]
-	`say hi #{@name}, my name is bob. what would you like to talk about`
+	`say -v #{cookies[:buddy]} hi #{@name}! What would you like to talk about?`
 	redirect '/menu'
 end
 
